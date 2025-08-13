b7a505883311902177092feeb802504270451045
KaramPark <com3892@gmail.com>
2025-08-13 15:13:05 +0900

    feature : backend

--- backend/src/main/java/com/ram/backend/dto/TodoResponseDto.java
+++ backend/src/main/java/com/ram/backend/dto/TodoResponseDto.java
@@ -1,4 +1,34 @@
 package com.ram.backend.dto;
 
+import com.ram.backend.model.Todo;
+import com.ram.backend.repository.TodoRepository;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TodoResponseDto {
+    private Long id;
+    private String title;
+    private String description;
+    private Boolean completed;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public static TodoResponseDto fromEntity(Todo todo){
+        return new TodoResponseDto(
+                todo.getId(),
+                todo.getTitle(),
+                todo.getDescription(),
+                todo.getCompleted(),
+                todo.getCreatedAt(),
+                todo.getUpdatedAt()
+        );
+    }
 }
